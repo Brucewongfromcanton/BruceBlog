3a9348f288a0c763093d44b6c13a38b58693cd97
Brucewongfromcanton <brucewongfromcanton@gmail.com>
2025-03-05 16:44:50 +1100

    Initial basic template commit

---
+++ BruceBlog/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <li>About me</li>
+    <li>Projects</li>
+    <li>Gallery</li>
+    <li>Projects</li>
+  </div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+
+  li {
+    padding: 15px;
+    font-family: "Montserrat", serif;
+    font-size: 25px;
+    list-style-type: none;
+  }
+
+
+</style>
